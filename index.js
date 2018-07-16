1355316efcfd446a8f395140737eed0da69205ef
GCdM <gabrielcm.br@gmail.com>
2018-07-16 15:30:14 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (collection) {
+  const newArray = collection.slice(0, 2);
+  return newArray;
+}
+
+const returnLastTwoDrivers = function (collection) {
+  const newArray = collection.slice(-2);
+  return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(collection, fn) {
+  return fn(collection);
+}
